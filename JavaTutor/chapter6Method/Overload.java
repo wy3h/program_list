ea669a07237e280e033b17161be30e3dfc0a3202
kwkiyxgX <wy3hlelouch@gmail.com>
2017-09-08 10:59:25 +0800

    Chapter6Method

--- JavaTutor/chapter6Method/Overload.java
+++ JavaTutor/chapter6Method/Overload.java
@@ -2,18 +2,18 @@ package chapter6Method;
 
 public class Overload
 {
-	void f(int n)
+	void f(int n)	// parameter list is int
 	{
 		System.out.println("This is a f(int)");
 	}
-	void f(String s)
+	void f(String s) // parameter list is String
 	{
 		System.out.println("This is a f(String)");
 	}
 	public static void main(String[] args)
 	{
 		Overload o = new Overload();
-		o.f(1);
-		o.f("hello");
+		o.f(1);			// call f(int)
+		o.f("hello");	// call f(String)
 	}
 }
