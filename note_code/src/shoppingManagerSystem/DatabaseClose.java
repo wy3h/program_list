c7c5986f385fbc70cd4f25fb6763d97c923c1176
kwkiyxgX <wy3hlelouch@gmail.com>
2017-09-26 21:15:23 +0800

    DatabaseConnectAndClose

---
+++ note_code/src/shoppingManagerSystem/DatabaseClose.java
@@ -0,0 +1,64 @@
+package shoppingManagerSystem;
+
+import java.sql.*;
+
+public class DatabaseClose
+{
+	public static void addClose(PreparedStatement pstmt, Connection conn)
+	{
+		try
+		{
+			if (pstmt != null)
+			{
+				pstmt.close();
+			}
+		} catch (SQLException e1)
+		{
+			e1.printStackTrace();
+		}
+		try
+		{
+			if (conn != null)
+			{
+				conn.close();
+			}
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void queryClose(PreparedStatement pstmt, ResultSet rs, Connection conn)
+	{
+		try
+		{
+			if (pstmt != null)
+			{
+				pstmt.close();
+			}
+		} catch (SQLException e1)
+		{
+			e1.printStackTrace();
+		}
+		try
+		{
+			if (rs != null )
+			{
+				rs.close();
+			}
+		} catch (SQLException e1)
+		{
+			e1.printStackTrace();
+		}
+		try
+		{
+			if (conn != null)
+			{
+				conn.close();
+			}
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
