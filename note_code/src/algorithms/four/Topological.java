5547202f0c5cdc3b323d0068b2c8d715689d4e03
kwkiyxgX <wy3hlelouch@gmail.com>
2017-10-29 10:31:51 +0800

    Topological

---
+++ note_code/src/algorithms/four/Topological.java
@@ -0,0 +1,45 @@
+package algorithms.four;
+
+import edu.princeton.cs.algs4.DepthFirstOrder;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.SymbolDigraph;
+
+public class Topological
+{
+	private Iterable<Integer> order;
+	
+	public Topological(Digraph G)
+	{
+		DirectedCycle cyclefinder = new DirectedCycle(G);
+		if(!cyclefinder.hasCycle())
+		{
+			DepthFirstOrder dfs = new DepthFirstOrder(G);
+			order = dfs.reversePost();
+		}
+	}
+	
+	public Iterable<Integer> order()
+	{
+		return order;
+	}
+	
+	public boolean isDAG()
+	{
+		return order != null;
+	}
+	
+	public static void main(String[] args)
+	{
+		String filename = args[0];
+		String separator = args[1];
+		SymbolDigraph sg = new SymbolDigraph(filename, separator);
+		
+		Topological top =  new Topological(sg.G());
+		
+		for(int v : top.order())
+			StdOut.println(sg.name(v));
+	}
+	
+}
