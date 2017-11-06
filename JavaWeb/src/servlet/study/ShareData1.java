f02156064e5d23ad53c811575afc14f351beb052
kwkiyxgX <wy3hlelouch@gmail.com>
2017-11-06 15:15:54 +0800

    By servlet's Config object to share data between servlets.

---
+++ JavaWeb/src/servlet/study/ShareData1.java
@@ -0,0 +1,48 @@
+package servlet.study;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo1
+ */
+@WebServlet("/Demo1")
+public class ShareData1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShareData1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String data = "Hello,world!";
+		ServletContext context = this.getServletConfig().getServletContext();
+		
+		context.setAttribute("data", data);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
