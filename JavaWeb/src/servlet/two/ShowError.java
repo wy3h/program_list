1e76b4e8eed9187b6d188eaa08e77b70ccc27348
kwkiyxgX <wy3hlelouch@gmail.com>
2017-12-05 21:39:57 +0800

    ShowError

---
+++ JavaWeb/src/servlet/two/ShowError.java
@@ -0,0 +1,46 @@
+package servlet.two;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowError
+ */
+@WebServlet("/ShowError")
+public class ShowError extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ShowError()
+	{
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		response.sendError(407,"Need authentication!!!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		doGet(request, response);
+	}
+
+}
