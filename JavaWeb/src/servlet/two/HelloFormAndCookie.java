a059da42c928e6c1f975e3fc28b89399d735e694
kwkiyxgX <wy3hlelouch@gmail.com>
2017-12-08 15:44:47 +0800

    HelloFormAndCookieSend

--- JavaWeb/src/servlet/two/HelloFormAndCookie.java
+++ JavaWeb/src/servlet/two/HelloFormAndCookie.java
@@ -35,36 +35,30 @@ public class HelloFormAndCookie extends HttpServlet
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException
 	{
-		Cookie name = new Cookie("name", 
-				URLEncoder.encode(request.getParameter("name"), "UTF-8"));
-		Cookie url = new Cookie("url", 
-				request.getParameter("url"));
 		
-		name.setMaxAge(60*60*24);
-		url.setMaxAge(60*60*24);
-		
+		Cookie name =new Cookie("name", URLEncoder.encode(
+				request.getParameter("name"), "UTF-8"));
+		Cookie url = new Cookie("url", request.getParameter("url"));
+
+		name.setMaxAge(60 * 60 * 24);
+		url.setMaxAge(60 * 60 * 24);
+
 		response.addCookie(name);
 		response.addCookie(url);
-		
-		response.setContentType("text/html;charset=UTF-8");
-		
-		 PrintWriter out = response.getWriter();
-	        String title = "设置 Cookie 实例";
-	        String docType = "<!DOCTYPE html>\n";
-	        out.println(docType +
-	                "<html>\n" +
-	                "<head><title>" + title + "</title></head>\n" +
-	                "<body bgcolor=\"#f0f0f0\">\n" +
-	                "<h1 align=\"center\">" + title + "</h1>\n" +
-	                "<ul>\n" +
-	                "  <li><b>站点名：</b>："
-	                + request.getParameter("name") + "\n</li>" +
-	                "  <li><b>站点 URL：</b>："
-	                + request.getParameter("url") + "\n</li>" +
-	                "</ul>\n" +
-	                "</body></html>");
-	}
 
+		response.setContentType("text/html;charset=UTF-8");
+		String n = new String(request.getParameter("name").getBytes("ISO8859-1"), "UTF-8");
+		PrintWriter out = response.getWriter();
+		String title = "设置 Cookie 实例";
+		String docType = "<!DOCTYPE html>\n";
+		out.println(docType + "<html>\n" + "<head><title>" + title
+				+ "</title></head>\n" + "<body bgcolor=\"#f0f0f0\">\n"
+				+ "<h1 align=\"center\">" + title + "</h1>\n" + "<ul>\n"
+				+ "  <li><b>站点名：</b>：" + n
+				+ "\n</li>" + "  <li><b>站点 URL：</b>："
+				+ request.getParameter("url") + "\n</li>" + "</ul>\n"
+				+ "</body></html>");
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
