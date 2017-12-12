b7521f7620ecceb3355735800a790edbb3e961b1
kwkiyxgX <wy3hlelouch@gmail.com>
2017-12-12 21:41:42 +0800

    SessionTrack

---
+++ JavaWeb/src/servlet/two/SessionTrack.java
@@ -0,0 +1,93 @@
+package servlet.two;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionTrack
+ */
+@WebServlet("/SessionTrack")
+public class SessionTrack extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SessionTrack()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		HttpSession session = request.getSession();
+		Date createTime = new Date(session.getCreationTime());
+		Date lastAccessTime = new Date(session.getLastAccessedTime());
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		String title = "Servlet Session";
+		Integer visitCount = new Integer(0);
+		String visitCountKey = new String("visitCount");
+		String userIDKey = new String("userID");
+		String userID = new String("Runoob");
+
+		if (session.isNew())
+		{
+			title = "Servlet Session-new";
+			session.setAttribute(userIDKey, userID);
+		} else
+		{
+			visitCount = (Integer) session.getAttribute(visitCountKey);
+			visitCount = visitCount + 1;
+			userID = (String) session.getAttribute(userID);
+		}
+		session.setAttribute(visitCountKey, visitCount);
+
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		String docType = "<!DOCTYPE html>\n";
+		out.println(docType + "<html>\n" + "<head><title>" + title
+				+ "</title></head>\n" + "<body bgcolor=\"#f0f0f0\">\n"
+				+ "<h1 align=\"center\">" + title + "</h1>\n"
+				+ "<h2 align=\"center\">Session 信息</h2>\n"
+				+ "<table border=\"1\" align=\"center\">\n"
+				+ "<tr bgcolor=\"#949494\">\n"
+				+ "  <th>Session 信息</th><th>值</th></tr>\n" + "<tr>\n"
+				+ "  <td>id</td>\n" + "  <td>" + session.getId()
+				+ "</td></tr>\n" + "<tr>\n" + "  <td>创建时间</td>\n" + "  <td>"
+				+ df.format(createTime) + "  </td></tr>\n" + "<tr>\n"
+				+ "  <td>最后访问时间</td>\n" + "  <td>" + df.format(lastAccessTime)
+				+ "  </td></tr>\n" + "<tr>\n" + "  <td>用户 ID</td>\n" + "  <td>"
+				+ userID + "  </td></tr>\n" + "<tr>\n" + "  <td>访问统计：</td>\n"
+				+ "  <td>" + visitCount + "</td></tr>\n" + "</table>\n"
+				+ "</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		doGet(request, response);
+	}
+
+}
